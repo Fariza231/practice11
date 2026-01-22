d2eacd8bbb1a07f4cb0a65d46808eae49a69f5cb
Fariza231 <farizaarystanbek@gmail.com>
2026-01-22 23:52:12 +0500

    Add version endpoint

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
+
+app.get("/version", (req, res) => {
+  res.json({
+    version: "1.1",
+    updatedAt: "2026-01-20"
+  });
+});
+
 app.use("/items", require("./routes/items"));
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Mongo error:", err));
-
-app.get("/", (req, res) => {
-  res.json({ message: "API is running" });
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running");
